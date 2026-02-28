6867b6e56ab5e00509672957bebe76e9d8f1fdea
Hector Lugo <haze.lugo@gmail.com>
2026-02-27 23:19:32 -0600

    typos

--- schedule-comparison-tool/vite.config.js
+++ schedule-comparison-tool/vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools"; //not sure why this is here
-import tailwindcss from "@tailwindscss/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
