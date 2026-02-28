eb499649e6e2dfce783acaa88fb12ae69b760b03
Hector Lugo <haze.lugo@gmail.com>
2026-02-27 23:28:58 -0600

    added three mock students

---
+++ schedule-comparison-tool/src/data/mockStudents.js
@@ -0,0 +1,56 @@
+export const mockStudents = [
+  {
+    id: "101",
+    name: "Juan Botello",
+    schedule: [
+      { period: 1, course: "Algebra II", teacher: "Ms. Guerra", room: "101" },
+      {
+        period: 2,
+        course: "English III",
+        teacher: "Ms. Rodriguez",
+        room: "204",
+      },
+      { period: 3, course: "Biology", teacher: "Ms. Garza", room: "312" },
+      { period: 4, course: "US History", teacher: "Mr. Pray", room: "110" },
+      { period: 5, course: "PE", teacher: "Coach Cano", room: "GYM" },
+      { period: 6, course: "Art II", teacher: "Ms. Alaniz", room: "405" },
+      { period: 7, course: "Spanish II", teacher: "Ms. Flores", room: "202" },
+    ],
+  },
+  {
+    id: "101",
+    name: "Juan Botello",
+    schedule: [
+      { period: 1, course: "Algebra II", teacher: "Ms. Guerra", room: "101" },
+      {
+        period: 2,
+        course: "English III",
+        teacher: "Ms. Rodriguez",
+        room: "204",
+      },
+      { period: 3, course: "Biology", teacher: "Ms. Garza", room: "312" },
+      { period: 4, course: "US History", teacher: "Mr. Pray", room: "110" },
+      { period: 5, course: "PE", teacher: "Coach Cano", room: "GYM" },
+      { period: 6, course: "Art II", teacher: "Ms. Alaniz", room: "405" },
+      { period: 7, course: "Spanish II", teacher: "Ms. Flores", room: "202" },
+    ],
+  },
+  {
+    id: "101",
+    name: "Juan Botello",
+    schedule: [
+      { period: 1, course: "Algebra II", teacher: "Ms. Guerra", room: "101" },
+      {
+        period: 2,
+        course: "English III",
+        teacher: "Ms. Rodriguez",
+        room: "204",
+      },
+      { period: 3, course: "Biology", teacher: "Ms. Garza", room: "312" },
+      { period: 4, course: "US History", teacher: "Mr. Pray", room: "110" },
+      { period: 5, course: "PE", teacher: "Coach Cano", room: "GYM" },
+      { period: 6, course: "Art II", teacher: "Ms. Alaniz", room: "405" },
+      { period: 7, course: "Spanish II", teacher: "Ms. Flores", room: "202" },
+    ],
+  },
+];
